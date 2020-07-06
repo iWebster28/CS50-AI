7ddc361b73df21cc9631df325e668b6d7e1c055f
iWebster28 <iWebster28@users.noreply.github.com>
2020-07-05 22:53:33 -0400

    minor changes

--- Project2/pagerank/testing.py
+++ Project2/pagerank/testing.py
@@ -13,3 +13,17 @@ page = "3.html"
 
 output = pr.transition_model(corpus, page, pr.DAMPING)
 print(output)
+
+
+# if len(sys.argv) != 2:
+#         sys.exit("Usage: python pagerank.py corpus")
+#     corpus = crawl(sys.argv[1])
+
+corpus = {"1.html": {"2.html"}, "2.html": {"1.html", "3.html"}, "3.html": {"2.html", "4.html"}, "4.html": {"2.html"}}
+
+page = "1.html"
+
+linked = set(
+            pg for pg in corpus # Check every page in corpus if it links to `page`
+            if page in corpus[pg] # if pg contains any links to `page`!
+        )
